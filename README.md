14d200993bc644905ae0b16114a02a9ddc9f2334
corvuscorae <corvuscorae@gmail.com>
2025-05-27 13:43:48 -0700

    print results to webpage

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## lore generator 
 generates a world's lore based on time period and main form of government
 
+#### lore_keys.json
+contains the keys (or sub-categories) for each broad category in this lore generator.
+
 ### instructions
 - press R to generate a new lore base
-- open console to see output
--- main.js
+++ main.js
@@ -1,26 +1,62 @@
 let loreData;
 
-fetch("./loreos.json").then(
+fetch("./lore_keys.json").then(
     (response) => response.json()
 ).then(
     (json) => {
         loreData = json;
-        generateLoreBase(loreData);
+        generateLore(loreData.world, "world");
+        generateLore(loreData.continent, "continent");
     }
 );
 
-function generateLoreBase(dat){
+function generateLore(data, level){
     const base = {};
-    for(let pillar in dat){
-        const randomIndex = Math.floor(Math.random() * (dat[pillar].length - 1));
-        base[pillar] = dat[pillar][randomIndex];
+
+    for(let cat in data){
+        let numPicks = Math.floor(Math.random() * cat[0])
+        base[cat] = new Set();
+
+        // TODO: prevent randomIndex from repeating
+        for(let i = 0; i <= numPicks; i++){
+            if(data[cat].length === 0){ break; }
+            if(data[cat].length < numPicks){ numPicks = data[cat.length]; }
+
+            const randomIndex = Math.floor(Math.random() * (data[cat].length));
+            const pick = data[cat][randomIndex];
+            base[cat].add(pick);
+        }
     }
-    console.log(base)
+    
+    //console.log(base);
+    printLore(base, level);
+}
+
+function printLore(base, level){
+    let printTo = document.getElementById(`${level.toLowerCase()}-stats`)
+    let printOut = `<h3>${level.toUpperCase()} STATS</h3>`;
+
+    for(let b in base){
+        let b_items = "";
+
+        for(let item of base[b]){
+            b_items += `${item}, `
+        }
+        b_items = b_items.slice(0, -2); // remove ", "
+
+        printOut += `<p><b>${b.substring(2)}</b>: ${b_items}`;
+    }
+    //printOut += `<p><b>${pillar.substring(2)}</b>: ${dat[pillar][randomIndex]}</p>`
+    printTo.innerHTML = printOut;
 }
 
 // load new lore base
 document.addEventListener('keydown', (e) => {
 	if(e.key.toLowerCase() === "r"){
-        generateLoreBase(loreData)
+        generateLore(loreData.continent, "continent");
+	}
+    if(e.key.toLowerCase() === "w"){
+        generateLore(loreData.world, "world");
+        generateLore(loreData.continent, "continent");
 	}
 });
