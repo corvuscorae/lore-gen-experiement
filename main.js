288af97df732f19f23f98fb8f47dc5db8bf6ccf2
corvuscorae <corvuscorae@gmail.com>
2025-06-01 14:05:06 -0700

    refactor: separate lore gen stuff and webpage stuff

--- main.js
+++ main.js
@@ -1,208 +1,59 @@
-const JSON_PATH = "./json"
-let loreData;
+import GLOBAL from "./globals.js";
+import { generateWorld, genMultipleContinents, trimContinents } from "./lore.js";
 
-let worldStats;
-let continentStats = {};
-
-let numContinents = parseInt(document.getElementById("num-continents").value);
 const updateNumContinentsButton = document.getElementById("num-continents-button");
-updateNumContinentsButton.addEventListener('click', function() {
-    const oldNum = numContinents;
-    numContinents = parseInt(document.getElementById("num-continents").value);
+updateNumContinentsButton.addEventListener('click', async function() {
+    const oldNum = GLOBAL.NUM_CONTINENTS;
+    GLOBAL.NUM_CONTINENTS = parseInt(document.getElementById("num-continents").value);
 
-    if(oldNum < numContinents){
-        genMultipleContinents(loreData, numContinents, oldNum);
-    } else if (oldNum > numContinents){
-        trimContinents(oldNum - numContinents);
+    if(oldNum < GLOBAL.NUM_CONTINENTS){
+        await genMultipleContinents(GLOBAL.LORE_DATA, GLOBAL.NUM_CONTINENTS, oldNum);
+        printContinents();
+    } else if (oldNum > GLOBAL.NUM_CONTINENTS){
+        trimContinents(oldNum - GLOBAL.NUM_CONTINENTS);
+        printContinents();
     }
 });
 
-fetch(`${JSON_PATH}/_loreKeys.json`).then(
+fetch(`${GLOBAL.JSON_PATH}/_loreKeys.json`).then(
     (response) => response.json()
 ).then(
     (json) => {
-        loreData = json;
-        init(loreData, numContinents)
+        GLOBAL.LORE_DATA = json;
+        init(GLOBAL.LORE_DATA, GLOBAL.NUM_CONTINENTS);
     }
 );
 
+// load new lore base
+document.addEventListener('keydown', (e) => {
+	if(e.key.toLowerCase() === "r"){
+        document.getElementById(`continent-stats`).innerHTML = "";
+        genMultipleContinents(GLOBAL.LORE_DATA, GLOBAL.NUM_CONTINENTS);
+        printContinents();
+    }
+    if(e.key.toLowerCase() === "w"){
+        init(GLOBAL.LORE_DATA, GLOBAL.NUM_CONTINENTS);
+        printWorld();
+	}
+});
+
 async function init(loreData, num){
-    worldStats = await generateLore(loreData.world);
+    await generateWorld(loreData, num);
+
+    printWorld();
+    printContinents();
+}
+
+function printWorld(){
     document.getElementById(`world-stats`).innerHTML = "";
-    printLore(worldStats, "world");
-    
+    printLore(GLOBAL.WORLD_STATS, "world");
+}
+
+function printContinents(){
     document.getElementById(`continent-stats`).innerHTML = "";
-    genMultipleContinents(loreData, num);
-}
-
-async function genMultipleContinents(loreData, num, from = 0) {
-    num = parseInt(num);
-    from = parseInt(from);
-
-    for(let i = from; i < num; i++){
-        let newContinent = await genContinent(loreData, i);
-        continentStats[newContinent.name] = newContinent;
+    for(let continent in GLOBAL.CONTINENT_STATS){
+        printLore(GLOBAL.CONTINENT_STATS[continent], "continent")
     }
-
-    // make relationships between continents
-    // TODO: implement smarter relationship generation (maybe based on governments/ideology)
-    for(let c in continentStats){
-        if(continentStats[c].government[0] !== "none"){
-            if(continentStats[c].allies.size === 0){
-                getRandomAllies(
-                    continentStats[c], 
-                    Math.floor(Math.random() * num)
-                );
-            }
-            if(continentStats[c].enemies.size === 0){
-                getRandomEnemies(
-                    continentStats[c], 
-                    Math.floor(Math.random() * num)
-                );
-            }
-        }
-    }
-
-    for(let c in continentStats){
-        printLore(continentStats[c], "continent");
-    }
-
-    // DEBUG: console.log(continentStats)
-}
-
-function getRandomAllies(self, num){
-    const allies = self.allies;
-    const  cKeys = Object.keys(continentStats);
-    let randomIndex = Math.floor(Math.random() * cKeys.length);
-
-    for(let i = 0; i < num; i++){
-        let potentialAlly = continentStats[cKeys[randomIndex]]; 
-        if( potentialAlly.ID !== self.ID && 
-            potentialAlly.government[0] !== "none" &&
-            !self.enemies.has(potentialAlly.ID) &&
-            !potentialAlly.enemies.has(self.ID)
-        ){
-            allies.add(potentialAlly.ID);
-            potentialAlly.allies.add(self.ID)           
-        }   
-        randomIndex = Math.floor(Math.random() * cKeys.length);
-    }
-}
-
-function getRandomEnemies(self, num){
-    const enemies = self.enemies;
-    const  cKeys = Object.keys(continentStats);
-    let randomIndex = Math.floor(Math.random() * cKeys.length);
-
-    for(let i = 0; i < num; i++){
-        let potentialEnemy = continentStats[cKeys[randomIndex]]; 
-        if( potentialEnemy.ID !== self.ID &&
-            potentialEnemy.government[0] !== "none" &&
-            !self.allies.has(potentialEnemy.ID) &&
-            !potentialEnemy.allies.has(self.ID)
-        ){
-            enemies.add(potentialEnemy.ID);
-            potentialEnemy.enemies.add(self.ID)           
-        }   
-        randomIndex = Math.floor(Math.random() * cKeys.length);
-    }
-}
-
-async function genContinent(loreData, i){
-    let continent = await generateLore(loreData.continent);
-    continent.name = [`continent ${i+1}`];  // TODO: name continents
-    continent.ID = [i+1];
-    continent.allies = new Set();
-    continent.enemies = new Set();
-
-    return continent;
-}
-
-function trimContinents(num){
-    // trim the last {num} keys
-    const trimKeys = Object.keys(continentStats).slice(-num);
-    for(const key of trimKeys){
-        delete continentStats[key];
-    }
-
-    // update webpage to no longer show trimmed continents
-    document.getElementById(`continent-stats`).innerHTML = "";
-    for(let continent in continentStats){
-        printLore(continentStats[continent], "continent")
-    }
-}
-
-async function generateLore(data){
-    const base = {};
-
-    for(let cat in data){
-        const maxPicks = data[cat].maxPicks;
-        const minPicks = (data[cat].minPicks) ? data[cat].minPicks : 0;
-        const numPicks = Math.ceil(Math.random() * (maxPicks - minPicks)) + minPicks;
-
-        base[cat] = new Set();
-        let choices;
-        if(data[cat].length !== 0){ 
-            choices = await getChoices(data, cat, base);
-        }
-        // TODO: LEFT OFF HERE
-        // debug: why is revered choices not gathering all possible choices (or IS it??)
-
-        // TODO: prevent randomIndex from repeating
-        for(let i = 0; i < numPicks; i++){
-            if(choices === null){
-                console.error(`ERROR: unable to load choices for {${cat}}`);
-                break;
-            }
-
-            if(data[cat].length < numPicks){ numPicks = choices.length; }
-
-            const randomIndex = Math.floor(Math.random() * (choices.length));
-            let pick = choices[randomIndex];
-
-            if(pick && pick !== "none" && data[cat].modifiers){
-                let mods = data[cat].modifiers.values;
-                // TODO ADD WEIGHTING CONTROL FOR MODS!!!!!!!! 
-                const randomIndex = Math.floor(Math.random() * (mods.length));
-                const modifier = mods[randomIndex];
-
-                if(modifier.length > 0){ pick = modifier + " " + pick; }
-            }
-
-            base[cat].add(pick);
-        }
-        base[cat] = [...base[cat]]; // convert to array
-    }
-    
-    //console.log(base);
-    return base;
-}
-
-async function getChoices(data, cat, self){
-    let choices = [];
-    if(data[cat].choice_control){
-        const controller = data[cat].choice_control;
-        const choiceJSON = await loadJSON(`${JSON_PATH}/${controller.json}.json`);
-
-        let loc;
-        let attr_choices;
-        if(controller.location === "world"){ loc = worldStats; }
-        else if(controller.location === "self"){ loc = self; }
-        else {
-            console.error(`Unknown control location: ${controller.location}`)
-            return;
-        }
-
-        attr_choices = loc[`${controller.attribute}`]; 
-        for(const c in attr_choices){
-            choices = choices.concat(choiceJSON[attr_choices[c]]); 
-        }
-    }
-    else if(data[cat].choices){
-        choices = data[cat].choices;  
-    }
-            
-    return choices;
 }
 
 function printLore(base, level){
@@ -222,28 +73,3 @@ function printLore(base, level){
     //printOut += `<p><b>${pillar.substring(2)}</b>: ${dat[pillar][randomIndex]}</p>`
     printTo.innerHTML += printOut;
 }
-
-async function loadJSON(filePath) {
-    try {
-        const response = await fetch(filePath);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Could not load JSON: " + error);
-        return null;
-    }
-}
-
-// load new lore base
-document.addEventListener('keydown', (e) => {
-	if(e.key.toLowerCase() === "r"){
-        document.getElementById(`continent-stats`).innerHTML = "";
-        genMultipleContinents(loreData, numContinents);
-    }
-    if(e.key.toLowerCase() === "w"){
-        init(loreData, numContinents);
-	}
-});
